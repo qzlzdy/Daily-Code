821455f36347064e06dee792bb99c1a0e08fa56c
qzlzdy <>
2021-08-08 08:18:20 +0800

    UPDATE Setu 21-8-8

--- Setu/src/encode.cpp
+++ Setu/src/encode.cpp
@@ -17,15 +17,20 @@ int main(int argc, char **argv){
         string stem = filename.stem().string();
         string extension = filename.extension().string();
         switch(stem.front()){
-        case 'H':
         case 'G':
         case 'V':
+            break;
+        case 'H':
+            if(stem[1] == '0'){
+                fs::remove(root / filename);
+                cout << "remove file " << filename.string() << endl;
+            }
             break;
         default:{
             fs::path oldname = root / filename;
             fs::path newname =
                 root / fs::path("H" + to_string(idx++) + extension);
-            // fs::rename(oldname, newname);
+            fs::rename(oldname, newname);
             cout << "rename " << oldname.string()
                  << " as " << newname.string() << endl;
             break;
