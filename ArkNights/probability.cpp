971d0242e1a49d3f9383f4b8fa17cb4f5e34a298
Qzlzdy <qzlzdy@qq.com>
2021-10-23 00:34:14 +0800

    UPDATE Arknights probability 21-10-23

---
+++ ArkNights/probability.cpp
@@ -0,0 +1,94 @@
+#include <array>
+#include <iomanip>
+#include <iostream>
+#include <mutex>
+#include <vector>
+
+using namespace std;
+
+// 六星出率
+static double getSixProb(int Count){
+    if(Count <= 50){
+        return 0.02;
+    }
+    return 0.02 * Count - 0.98;
+}
+
+// 第X首次出六星
+static double getFirstSixProb(int Count){
+    static array<double, 99> FirstProb;
+    static bool InitFirstProb = false;
+    if(InitFirstProb){
+        return FirstProb[Count - 1];
+    }
+    double t = 1;
+    for(int i = 1; i < 100; ++i){
+        FirstProb[i - 1] = t * getSixProb(i);
+        t *= 1 - getSixProb(i);
+    }
+    InitFirstProb = true;
+    return FirstProb[Count - 1];
+}
+
+// 第Y首次出限定
+static double getFirstSpecProb(int Count, double a){
+    static array<double, 299> FirstProb;
+    static bool InitFirstProb = false;
+    if(InitFirstProb){
+        return FirstProb[Count - 1];
+    }
+    for(int i = 1; i < 300; ++i){
+        double prob = 0;
+        for(int j = 1; j < 100 && i - j > 0; ++j){
+            prob += FirstProb[i - j - 1] * getFirstSixProb(j);
+        }
+        prob *= 1 - a;
+        prob += i < 99 ? getFirstSixProb(i) * a : 0;
+        FirstProb[i - 1] = prob;
+    }
+    InitFirstProb = true;
+    return FirstProb[Count - 1];
+}
+
+double E(vector<double> &Prob){
+    double res = 0;
+    for(int i = 0; i < Prob.size(); ++i){
+        res += Prob[i] * (i + 1);
+    }
+    return res;
+}
+
+double D(vector<double> &Prob){
+    double EX = E(Prob);
+    double res = 0;
+    for(int i = 0; i < Prob.size(); ++i){
+        res += Prob[i] * (i - EX + 1) * (i - EX + 1);
+    }
+    return res;
+}
+
+int main(){
+    vector<double> Prob(300);
+    double sum = 0;
+    for(int i = 1; i < 300; ++i){
+        Prob[i - 1] = getFirstSpecProb(i, 0.35);
+        sum += Prob[i - 1];
+    }
+    Prob[299] = 1 - sum;
+
+    // Display
+    for(int i = 0; i < 75; ++i){
+        cout << "P{Y=" << 4 * i + 1 << "} = "
+             << fixed << setprecision(6) << Prob[4 * i] << "\t";
+        cout << "P{Y=" << 4 * i + 2 << "} = "
+             << fixed << setprecision(6) << Prob[4 * i + 1] << "\t";
+        cout << "P{Y=" << 4 * i + 3 << "} = "
+             << fixed << setprecision(6) << Prob[4 * i + 2] << "\t";
+        cout << "P{Y=" << 4 * i + 4 << "} = "
+             << fixed << setprecision(6) << Prob[4 * i + 3];
+        cout << endl;
+    }
+    cout << "E(X) = " << fixed << setprecision(6) << E(Prob) << endl;
+    cout << "D(X) = " << fixed << setprecision(6) << D(Prob) << endl;
+    return 0;
+}
--- Setu/script/crop.py
+++ Setu/script/crop.py
@@ -1,27 +1,33 @@
 import cv2
 import sys
 import os.path
-import sqlite3
-
-db = sqlite3.connect('../Setu.db')
+from PIL import Image
 
 def getPath(illust_id):
     path = '/run/media/qzlzdy/Data/qzlzdy/setu/H{id}.{ext}'
-    cur = db.execute('SELECT extension FROM Illustrations WHERE id = ?', (illust_id,))
-    return path.format(id=illust_id, ext=cur.fetchone()[0])
+    extensions = [
+        'jpg', 'png', 'bmp', 'jpeg', 'JPEG'
+    ]
+    for ext in extensions:
+        try:
+            probe = Image.open(path.format(id=illust_id, ext=ext))
+            return path.format(id=illust_id, ext=ext)
+        except:
+            pass
+    return None
 
 def detect(fileid, cascade_file = "./lbpcascade_animeface.xml"):
     cascade = cv2.CascadeClassifier(cascade_file)
     image = cv2.imread(getPath(fileid), cv2.IMREAD_COLOR)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     gray = cv2.equalizeHist(gray)
-    
+
     faces = cascade.detectMultiScale(gray,
                                      # detector options
                                      scaleFactor = 1.1,
                                      minNeighbors = 1,
                                      minSize = (192, 192))
-    outname = '/home/qzlzdy/Python/datasets/setu/{}-{}.jpg'
+    outname = '/home/qzlzdy/python/datasets/setu/{}-{}.jpg'
     i = 1
     for (x, y, w, h) in faces:
         width = len(image[0])
@@ -38,7 +44,7 @@ def detect(fileid, cascade_file = "./lbpcascade_animeface.xml"):
         i += 1
         #cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
 
-l = [21056, 24351, 24626] + list(range(24887, 25225))
+l = [10524, 20650, 24267, 24661, 24907, 25038] + list(range(25225, 25556))
 for i in l:
     print('cropping illustration', i)
     detect(i)
