3d1249c04b387702915e016869ca2fc2da10f787
qzlzdy <qzlzdy@qq.com>
2023-05-23 16:23:20 +0800

    ADD proxy

--- Trackers/merge.sh
+++ Trackers/merge.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+
+export https_proxy=http://127.0.0.1:7890
+
 cd trackerslist
 git pull
+
 cd ../TrackersListCollection
 git pull
+
 cd ..
 cat trackerslist/trackers_all.txt TrackersListCollection/all.txt > trackers.txt
-
